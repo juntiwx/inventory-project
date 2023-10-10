9a3b98505ce0babd15ffe7dd9438db3f1b644d29
juntiwx <juntima.wx@gmail.com>
2023-10-10 10:33:56 +0700

    add inertia and vue js to laravel project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return \Inertia\Inertia::render('App');
+//    return view('welcome');
 });
